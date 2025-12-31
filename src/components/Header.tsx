4cc8f0bba6ae7a134ad9aed7c837f987e948814f
Pixelprophett <nv787265@gmail.com>
2025-12-31 13:23:33 +0530

    Revert "update"
    This reverts commit 25fa5f9c96788ef23791613a1a7e13ac7d1b5e18.

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <header className="p-6 flex justify-between items-center relative z-10">
+            <div className="flex items-center gap-2">
+                <h1 className="font-display text-3xl text-primary leading-none">CRUNCHD</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
